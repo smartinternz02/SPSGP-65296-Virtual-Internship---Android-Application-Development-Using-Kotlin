6b58a8bf054a8dcaa4f3afed068f1ab832cc2a84
Shahzad Sayyed <98218245+Shahzad1729@users.noreply.github.com>
2022-09-18 18:59:39 +0530

    Add files via upload

---
+++ GrocerryApp - Main Project/app/src/main/java/com/example/groceryapplication/GroceryRVAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.groceryapplication
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class GroceryRVAdapter(var list: List<GroceryItems>, private val groceryItemClickInterface: GroceryItemClickInterface): RecyclerView.Adapter<GroceryRVAdapter.GroceryViewHolder>() {
+
+    inner class GroceryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val nameTV: TextView = itemView.findViewById(R.id.idTVItemName)
+        val quantityTV: TextView = itemView.findViewById(R.id.idTVQuantity)
+        val rateTV: TextView = itemView.findViewById(R.id.idTVRate)
+        val amountTV: TextView = itemView.findViewById(R.id.idTVTotalAmt)
+        val deleteTV: ImageView = itemView.findViewById(R.id.idTVDelete)
+    }
+
+    interface GroceryItemClickInterface{
+        fun onItemClick(groceryItems: GroceryItems)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.grocery_rv_item, parent, false)
+        return GroceryViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: GroceryViewHolder, position: Int) {
+        holder.nameTV.text = list[position].itemName
+        holder.quantityTV.text = list[position].itemQuantity.toString()
+        holder.rateTV.text = "Rs. "+ list[position].itemPrice.toString()
+        val itemTotal: Int = list[position].itemPrice * list[position].itemQuantity
+        holder.amountTV.text = "Rs. $itemTotal"
+        holder.deleteTV.setOnClickListener{
+            groceryItemClickInterface.onItemClick(list[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
